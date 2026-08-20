aaced4e1b557b4dd59a7d09040e8ff800cef8c02
Will de la Bretonne <ldelab2@outlook.com>
2026-08-19 19:06:01 -0500

    creating app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-3xl font-bold text-white underline">
+        Hello World 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
